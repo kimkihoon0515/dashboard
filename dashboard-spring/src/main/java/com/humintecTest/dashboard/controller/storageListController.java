ebc415c12ce7b39c83df53252c6f2578278e67d3
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2021-07-06 14:51:27 +0900

    [개발환경설정]db연동 및 spring 설정

---
+++ dashboard-spring/src/main/java/com/humintecTest/dashboard/controller/storageListController.java
@@ -0,0 +1,26 @@
+package com.humintecTest.dashboard.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.humintecTest.dashboard.service.StorageListService;
+import com.humintecTest.dashboard.vo.StorageListVo;
+
+@RestController
+public class storageListController {
+	@Autowired
+	StorageListService storageListService;
+	
+	@RequestMapping("/storageList")
+	public @ResponseBody List<StorageListVo> selectStorageList(@ModelAttribute("searchVo") StorageListVo vo, ModelMap modelMap) throws Exception{
+		List<StorageListVo> vList = storageListService.selectStorageList(vo);
+		
+		return vList;
+	}
+}
