0f1a75bacd089dd3196786668336404ab846e0d5
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2021-07-08 14:03:14 +0900

    [3.2] frontend와 backend 연결 및 CORS 해결

--- dashboard-vue/src/router/index.js
+++ dashboard-vue/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sample',
+      name: 'Sample',
+      component: () => import(/* webpackChunkName: "about" */ '../views/Sample.vue')
     }
   ]
 })
