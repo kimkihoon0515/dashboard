9b2c46c3e57a3421d8bbad6db844a01e7800f46b
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2021-07-21 13:28:24 +0900

    no message

--- dashboard-spring/src/main/java/com/humintecTest/dashboard/controller/StorageFreeController.java
+++ dashboard-spring/src/main/java/com/humintecTest/dashboard/controller/StorageFreeController.java
@@ -20,43 +20,6 @@ public class StorageFreeController {
     @Autowired
     StorageFreeService storageFreeService;
 
-    @GetMapping("/selectStorageFree")
-    @CrossOrigin(origins = "*")
-    public List<storageFreeResponseFormat> selectFreeStorage() {
-        StorageFreeVo vo1 = new StorageFreeVo();
-        List<StorageFreeVo> vList = storageFreeService.selectStorageFree(vo1);
-        ArrayList<storageFreeResponseFormat> res = new ArrayList<storageFreeResponseFormat>();
-
-        for (StorageFreeVo target : vList) {
-            res.add(new storageFreeResponseFormat(target));
-        }
-        return res;
-    }
-
-    @PutMapping("/insertStorageFree")
-    @CrossOrigin(origins = "*")
-    public String insertStorageFree(StorageFreeVo vo) {
-        List<StorageFreeVo> vList = storageFreeService.selectStorageFree(vo);
-
-        for (StorageFreeVo target : vList) {
-            if (storageFreeService.insertStorageFree(target) == 0) {
-
-            } else
-                return "false";
-        }
-        return "ok";
-    }
-
-    @PutMapping("/deleteStorageFree")
-    @CrossOrigin(origins = "*")
-    public String deleteStorageFree(StorageFreeVo vo) {
-        if (storageFreeService.deleteStorageFree() == 0) {
-        } else {
-            return "false";
-        }
-        return "ok";
-    }
-
     @GetMapping("/searchStorageFreeByDate")
     @Transactional(readOnly = true)
     @CrossOrigin(origins = "*")
