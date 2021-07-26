2c5ea82b20830586e47819c99335832adb551149
DESKTOP-0RDFKIA\dnrla <dnrlalth@gmail.com>
2021-07-26 18:00:18 +0900

    no message

--- dashboard-spring/src/main/java/com/humintecTest/dashboard/service/ScannerTableService.java
+++ dashboard-spring/src/main/java/com/humintecTest/dashboard/service/ScannerTableService.java
@@ -1,5 +1,13 @@
 package com.humintecTest.dashboard.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.humintecTest.dashboard.dao.ScannerTableDao;
+
 public class ScannerTableService {
+	@Autowired
+	ScannerTableDao scannerTableDao;
+	
+	public 
 
 }
